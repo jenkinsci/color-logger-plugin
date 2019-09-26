fc9f3f506c26de4dc97897ce6eed8ee1e56f9ca8
Albert Lv <albert.lv@outlook.com>
2019-09-26 09:54:54 +0800

    Remove redundant filtering

--- src/main/java/io/jenkins/plugins/logger/LineUtils.java
+++ src/main/java/io/jenkins/plugins/logger/LineUtils.java
@@ -13,9 +13,7 @@ public class LineUtils {
 
   private static final Pattern WARNING = Pattern.compile("WARN", Pattern.CASE_INSENSITIVE);
   private static final Pattern ERROR = Pattern.compile("ERROR", Pattern.CASE_INSENSITIVE);
-  private static final Pattern CMD = Pattern.compile("\u001B\\[0(;[3-4][0-7])?m");
 
-  private static final String HIDDEN_CMD = "\u001B[8mha";
   private static final String WARNING_SPAN_PREFIX = "<span style=\"color: #fff; background-color: #f57542; font-size: 100%; font-weight: bold\">";
   private static final String ERROR_SPAN_PREFIX = "<span style=\"color: #fff; background-color: #ed0e0e; font-size: 100%; font-weight: bold\">";
   private static final String SPAN_SUFFIX = "</span>";
@@ -23,9 +21,6 @@ public class LineUtils {
   public static String filterLine(String line) {
     String result = null;
     try {
-      if (line.indexOf(HIDDEN_CMD) != -1) return result;
-      line = CMD.matcher(line).replaceAll("");
-
       StringBuilder sb = new StringBuilder(line);
       if (WARNING.matcher(line).find()) {
         sb.insert(0, WARNING_SPAN_PREFIX);
@@ -44,9 +39,6 @@ public class LineUtils {
   }
 
   public static void main(String[] args) {
-    Pattern CMD = Pattern.compile("\u001B\\[0(;[3-4][0-7])?m");
-    String cmd = "\u001B[0;31m";
-    System.out.println(CMD.matcher(cmd).matches());
   }
 
 }
